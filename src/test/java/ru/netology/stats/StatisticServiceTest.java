8fb86142191eda0f9fb0121b222c57edae52d638
Maria <ivovamaria1@gmail.com>
2023-04-22 23:51:40 +0600

    new commit

---
+++ src/test/java/ru/netology/stats/StatisticServiceTest.java
@@ -0,0 +1,18 @@
+package ru.netology.stats;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class StatisticServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
