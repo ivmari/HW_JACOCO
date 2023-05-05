5b9e8706169b368829a4b1c35129749c3c028187
Maria <ivovamaria1@gmail.com>
2023-05-05 22:18:12 +0600

    second commit

--- src/main/java/ru/netology/stats/StatisticService.java
+++ src/main/java/ru/netology/stats/StatisticService.java
@@ -1,2 +1,13 @@
-package ru.netology.stats;public class StatisticService {
+package ru.netology.stats;
+
+public class StatisticService {
+    public long findMax(long[] incomes) {
+        long currentMax = incomes[0];
+        for (long income : incomes) {
+            if (currentMax < income) {
+                currentMax = income;
+            }
+        }
+        return currentMax;
+    }
 }
--- src/test/java/ru/netology/stats/StatisticServiceTest.java
+++ src/test/java/ru/netology/stats/StatisticServiceTest.java
@@ -15,4 +15,40 @@ public class StatisticServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxSecond() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {113, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 113;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxNull() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {0, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxLongNumber() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {111111111, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 111111111;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
